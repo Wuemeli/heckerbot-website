61415bf25e1b23d2493d7c6027b75daf369f2686
Wuemeli <wuemeli@gmail.com>
2024-01-23 11:06:40 +0100

    Fix

--- server/api/proxy.ts
+++ server/api/proxy.ts
@@ -1,4 +1,6 @@
+import axios from 'axios';
+
 export default defineEventHandler(async (event) => {
-    const response = await fetch('http://paid1.daki.cc:4043/stats');
-    return response;
+    const response = await axios.get('http://paid1.daki.cc:4043/stats');
+    return response.data;
 });
