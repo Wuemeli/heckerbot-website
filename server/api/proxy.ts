fb27c4c81226961e36efe2fc46bdfd3ea8412cac
Wuemeli <wuemeli@gmail.com>
2024-01-23 10:44:29 +0100

    JSON Fix

--- server/api/proxy.ts
+++ server/api/proxy.ts
@@ -1,5 +1,4 @@
 export default defineEventHandler(async (event) => {
     const response = await fetch('http://paid1.daki.cc:4043/stats');
-    const data = await response.json();
-    return data;
+    return response;
 });
